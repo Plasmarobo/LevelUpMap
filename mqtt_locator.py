9bf7a3ce3c1a1d3da21961d3ab2447d696637a51
Steve Pomeroy <steve@staticfree.info>
2018-05-04 12:57:31 -0400

    New map style (#3)
    * Import map SVG
    
    * Update map image; style text
    
    * Typo
    
    * Add daily totals; switch to Python 3
    
    * Add comma to order count
    
    * Increase lifetime to 3
    
    * Animate the order total to make it more exciting
    
    * Add in collision-detection to avoid overlap
    
    * Add Box2D
    * Change animation to fade boxes instead of shrink them
    
    * Update requirements

--- mqtt_locator.py
+++ mqtt_locator.py
@@ -2,7 +2,11 @@
 
 from __future__ import print_function
 
-import ConfigParser
+try:
+    from ConfigParser import SafeConfigParser
+except ImportError:
+    from configparser import SafeConfigParser
+from datetime import datetime
 import json
 import random
 import sys
@@ -11,7 +15,8 @@ import time
 import paho.mqtt.client as mqtt
 import pygame
 import pyproj
-from animated_average import AnimatedAverage
+from animated_value import AnimatedAverage, AnimatedValue
+from Box2D import b2PolygonShape, b2World
 
 from uszipcode import ZipcodeSearchEngine
 
@@ -25,15 +30,17 @@ class Ping(object):
         (0x1E, 0xBB, 0xF3),
         (0x71, 0xCB, 0x3A)]
 
-    def __init__(self, x_loc, y_loc, text):
+    def __init__(self, world, x_loc, y_loc, text):
         self.created_time = time.time()
-        self.life_time = 1
+        self.life_time = 3
         self.color = random.choice(Ping.colors)
         self.size = 40
-        self.coordinate = [x_loc, y_loc]
         self._text = text
         self._text_surface = None
         self._text_surface2 = None
+        self._rect_surface = None
+        self._body = world.CreateDynamicBody(position=(x_loc, y_loc), fixedRotation=True)
+        self._box = self._body.CreatePolygonFixture(box=(self.size, self.size), density=1, friction=0.0)
 
     def is_alive(self):
         """Returns true if we are within lifetime, false otherwise"""
@@ -45,21 +52,32 @@ class Ping(object):
 
     def draw(self, win, font):
         """Renders a ping to a display window"""
-        sq_size = int(round((1 - self.life_factor()) * self.size))
-        center_square = (self.coordinate[0] - sq_size/2,
-                         self.coordinate[1] - sq_size/2,
-                         sq_size, sq_size)
-        pygame.draw.rect(win, self.color, center_square, 0)
+        pos = self._body.position
+
+        sq_size = self.size
+        center_square = (pos[0] - sq_size/2,
+                         pos[1] - sq_size/2)
+        alpha = int((1.0 - self.life_factor()) * 255)
+        if not self._rect_surface:
+            self._rect_surface = pygame.surface.Surface((sq_size, sq_size))
+            self._rect_surface.fill(self.color)
+        self._rect_surface.set_alpha(alpha)
+        win.blit(self._rect_surface, center_square)
         if not self._text_surface:
             self._text_surface = font.render(self._text, True, self._text_color)
             rect = self._text_surface.get_rect()
             self._text_surface.convert_alpha()
             self._text_surface2 = pygame.surface.Surface(rect.size, pygame.SRCALPHA, 32)
-            self._text_pos = (self.coordinate[0] - rect.width/2, self.coordinate[1])
+            self._text_width = rect.width
         fade = int(255 * (1 - self.life_factor()))
         self._text_surface2.fill((255, 255, 255, fade))
         self._text_surface2.blit(self._text_surface, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
-        win.blit(self._text_surface2, self._text_pos)
+        text_pos = (pos[0] - self._text_width/2, pos[1] + 25)
+        win.blit(self._text_surface2, text_pos)
+
+    def destroy(self, world):
+        if self._body:
+            world.DestroyBody(self._body)
 
     def __repr__(self):
         return "<Ping {}: {:.3f}, {:.3f}>".format(self.created_time,
@@ -72,15 +90,20 @@ class Map(object):
     _text_color = (0x55, 0x55, 0x55)
     background_color = (0xe9, 0xe9, 0xe9)
 
-
     def __init__(self, config):
         pygame.display.init()
         pygame.font.init()
+        self._world = b2World(gravity=(0, 0))
         screen_info = pygame.display.Info()
         pygame.mouse.set_visible(False)
         self.pings = []
         self.config = config
-        self._avg_spend = AnimatedAverage()
+        self._avg_spend = AnimatedAverage(count=500)
+        self._order_count = 0
+        self._cum_order_spend = 0
+        self._cum_order_spend_anim = AnimatedValue()
+        self._day_start = datetime.now()
+        self._last_frame = 0
 
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
@@ -89,7 +112,8 @@ class Map(object):
                             int(self.config["port"]),
                             int(self.config["keepalive"]))
 
-        self._font = pygame.font.SysFont('Source Sans Pro', 30)
+        self._font = pygame.font.SysFont('Source Sans Pro Semibold', 25)
+        self._font_avg_spend = pygame.font.SysFont('Source Sans Pro', 30, bold=True)
         self.background = pygame.image.load(config['map_image'])
 
         self.proj_in = pyproj.Proj(proj='latlong', datum='WGS84')
@@ -108,7 +132,7 @@ class Map(object):
                     self.background.get_width(),
                     self.background.get_height()
                 ],
-                pygame.NOFRAME)
+                pygame.NOFRAME | pygame.HWSURFACE | pygame.DOUBLEBUF)
             self.x_offset = self.y_offset = 0
         else:
             self.win = pygame.display.set_mode(
@@ -141,7 +165,7 @@ class Map(object):
         places = [seattle, la, bar_harbor, miami, left_coast, cape_flattery, west_quoddy, p_town]
         for place in places:
             (x_coord, y_coord) = self.project(*place)
-            self.pings.append(Ping(x_coord + self.x_offset, y_coord + self.y_offset, ''))
+            self.pings.append(Ping(self._world, x_coord + self.x_offset, y_coord + self.y_offset, ''))
 
     def on_connect(self, client, _flags, _userdata, response_code):
         """MQTT Connection callback"""
@@ -150,8 +174,8 @@ class Map(object):
         client.subscribe(self.config["topic"])
 
     def on_message(self, _client, _userdata, message):
-        """MQTT Message recieved callback"""
-        payload = json.loads(message.payload)
+        """MQTT Message received callback"""
+        payload = json.loads(message.payload.decode('utf-8'))
         if payload["postal_code"] is None or payload["postal_code"] == "":
             return
         if self.zips is None:
@@ -161,22 +185,40 @@ class Map(object):
             return
         merchant_name = payload.get('merchant_name', '')
         (x_coord, y_coord) = self.project(zcode["Longitude"], zcode["Latitude"])
-        self.pings.append(Ping(x_coord + self.x_offset, y_coord + self.y_offset, merchant_name))
+        self.pings.append(Ping(self._world, x_coord + self.x_offset, y_coord + self.y_offset, merchant_name))
         spend = int(payload['spend_amount'])
         if spend:
             self._avg_spend.add(spend)
+        self._maybe_reset_daily_totals()
+        self._order_count += 1
+        self._cum_order_spend += spend
+        self._cum_order_spend_anim.set(self._cum_order_spend)
+
+    def _draw_text_stat(self, text, value, index):
+        self.win.blit(self._font_avg_spend.render(text.format(value), True, self._text_color), (100, (self.win.get_height() - 180) + index * 40))
 
     def draw(self):
         """Render the map and it's pings"""
         self._avg_spend.tick()
+        self._cum_order_spend_anim.tick()
+        frame_time = pygame.time.get_ticks() / 1000
+        if self._last_frame:
+            frame_delay = frame_time - self._last_frame
+        else:
+            frame_delay = 1.0/60
+        self._world.Step(frame_delay, 6, 2)
+        self._last_frame = frame_time
         self.win.fill(Map.background_color)
         self.win.blit(self.background, (self.x_offset, self.y_offset))
         for ping in self.pings[:]:
             if ping.is_alive():
                 ping.draw(self.win, self._font)
             else:
+                ping.destroy(self._world)
                 self.pings.remove(ping)
-        self.win.blit(self._font.render("Avg. Spend  ${:0.02f}".format(self._avg_spend.get()/100.0), True, self._text_color), (20, self.win.get_height() - 60))
+        self._draw_text_stat("Average Order Price: ${:0.02f}", self._avg_spend.get()/100.0, 0)
+        self._draw_text_stat("Orders Today Total: ${:0,.02f}", self._cum_order_spend_anim.get()/100.0, 1)
+        self._draw_text_stat("Orders Today: {:,}", self._order_count, 2)
 
     def project(self, lon, lat):
         """Convert lat/long to pixel x/y"""
@@ -190,10 +232,17 @@ class Map(object):
         """Cleanup"""
         self.client.loop_stop()
 
+    def _maybe_reset_daily_totals(self):
+        now = datetime.now()
+        if self._day_start.day != now.day:
+            self._cum_order_spend = 0
+            self._order_count = 0
+            self._day_start = now
+
 
 def read_config(config_file):
     """Global function to read external config file"""
-    config = ConfigParser.SafeConfigParser()
+    config = SafeConfigParser()
     read = config.read(config_file)
     if not read:
         print("Could not read config file {}".format(config_file))
@@ -212,7 +261,6 @@ def main():
     done = False
     clock = pygame.time.Clock()
     world_map = Map(read_config(config_file))
-    avg_spend = AnimatedAverage(count=500)
 
     while not done:
         clock.tick(60)
